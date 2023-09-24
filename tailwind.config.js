0d52eee770d3a20560e888c611f90eec3a828660
Hasnat Rana <hxncodes@gmail.com>
2023-09-24 11:19:05 +0500

    Project initialized

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        publicSans: ["Public Sans"],
+      },
+      colors: {
+        darkerBlue: "hsl(233, 26%, 20%)",
+        dark: "hsl(233, 26%, 28%)",
+        darkBlue: "hsl(233, 26%, 24%)",
+        limeGreen: "hsl(136, 65%, 51%)",
+        brightCyan: "hsl(192, 70%, 51%)",
+        grayishBlue: "hsl(233, 8%, 62%)",
+        lightGrayishBlue: "hsl(220, 16%, 96%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "20px",
+          md: "50px",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
